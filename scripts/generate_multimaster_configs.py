6a8ad54fc8fe25085bf431255275b0eea721f7c2
abrinkmacmu <abrinkma@andrew.cmu.edu>
2017-01-19 11:53:55 -0500

    ready to test

--- scripts/generate_multimaster_configs.py
+++ scripts/generate_multimaster_configs.py
@@ -29,12 +29,17 @@ class MultimasterConfigGenerator:
 		self.robot_dict[self.roman_machine] = 'roman/'
 		# **new robots should go here**
 
+		self.actions = rospy.get_param('generator_commander_action_clients')
+		#IPython.embed()
+
 		self.defaultAdvertisements = defaultdict(list) # K-gateway, V-list of topics 
-		self.defaultPulls = defaultdict(list) # K-gateway, V-list of topics
+		self.defaultPullsTopics = defaultdict(list) # K-gateway, V-list of topics
+		self.defaultPullsGateway = defaultdict(list) # K-gateway, V-list of 'other_gateway'
 
 		for gw in self.gateway_list:
 			self.defaultAdvertisements[gw] = []
-			self.defaultPulls[gw] = []
+			self.defaultPullsTopics[gw] = []
+			self.defaultPullsGateway[gw] = []
 
 
 	def writeConfigHeader(self, file, gateway):
@@ -47,26 +52,76 @@ class MultimasterConfigGenerator:
 			file.write("network_interface: eth0\n")
 
 	def writeDefaultAdvertisements(self, file, gateway):
-		#TODO
 		file.write("default_advertisements:\n")
-		#for...
-		file.write("   - name: /" + publisher_topic +"\n")
-		file.write("     node: None\n")
-		file.write("     type: publisher\n")
-		rospy.loginfo("     topic: " + publisher_topic)
+		for topic in self.defaultAdvertisements[gateway]:
+			file.write("   - name: /" + topic +"\n")
+			file.write("     node: None\n")
+			file.write("     type: publisher\n")
+			rospy.loginfo("     topic: " + topic)
 
 	def writeDefaultPulls(self, file, gateway):
-		#TODO
 		file.write("\ndefault_pulls:\n")
-		#for ...
-		file.write("   - gateway: "+other_gateway+"_gateway\n")
-		file.write("     rule:\n")
-		file.write("       name: /"+topic+"\n")
-		file.write("       node: None\n")
-		file.write("       type: publisher\n\n")
+		for i in range(0, len(self.defaultPullsTopics[gateway])):
+			topic = self.defaultPullsTopics[gateway][i]
+			other_gateway = self.defaultPullsGateway[gateway][i]
+			file.write("   - gateway: "+other_gateway+"_gateway\n")
+			file.write("     rule:\n")
+			file.write("       name: /"+topic+"\n")
+			file.write("       node: None\n")
+			file.write("       type: publisher\n\n")
+
+	def writePublishedRemaps(self, file, gateway):
+		for topic in self.defaultAdvertisements[gateway]:
+			topic_unprefixed = topic[ topic.find('/')+1: len(topic)]
+			##topic_name = topic_unprefixed[0:topic_unprefixed.find('/')] #not unique enough
+			topic_name = topic_unprefixed.replace('/','_')
+			file.write("  <node name=\""+topic_name+"_relay_node\" pkg=\"topic_tools\" type=\"relay\" ")
+			file.write("args=\""+topic_unprefixed+" "+topic +"/>\n\n")
+
+
+	def writeSubscribedRemaps(self, file, gateway, robot_prefix):
+		for topic in self.defaultPullsTopics[gateway]:
+			if(topic.find(robot_prefix) > -1 ):
+				topic_unprefixed = topic[ topic.find('/')+1: len(topic)]
+				##topic_name = topic_unprefixed[0:topic_unprefixed.find('/')] #not unique enough
+				topic_name = topic_unprefixed.replace('/','_')
+				file.write("  <node name=\""+topic_name+"_relay_node\" pkg=\"topic_tools\" type=\"relay\" ")
+				file.write("args=\""+topic+" "+topic_unprefixed +"/>\n\n")
 
 	def run(self):
 		# first generate the list of default advertisements from actions
+
+		for robot in self.actions.keys():
+			for action_root in self.actions[robot]:
+				robot_machine = "unknown"
+				if robot == "pr2":
+					robot_machine = self.pr2_machine
+				elif robot == "roman":
+					robot_machine = self.roman_machine
+				else:
+					rospy.loginfo("Error parsing generator_commander_action_clients, unknown robot name")
+				## Add new robots here
+				
+
+				self.defaultAdvertisements[self.pr2_machine].append(robot+"/" + action_root + "/status")
+				self.defaultAdvertisements[self.pr2_machine].append(robot+"/" + action_root + "/result")
+				self.defaultAdvertisements[self.pr2_machine].append(robot+"/" + action_root + "/feedback")
+
+				self.defaultPullsTopics[self.pr2_machine].append(robot+"/" + action_root + "/goal")
+				self.defaultPullsGateway[self.pr2_machine].append(self.commander_machine)
+				self.defaultPullsTopics[self.pr2_machine].append(robot+"/" + action_root + "/cancel")
+				self.defaultPullsGateway[self.pr2_machine].append(self.commander_machine)
+
+				self.defaultAdvertisements[self.commander_machine].append(robot+"/" + action_root + "/goal")
+				self.defaultAdvertisements[self.commander_machine].append(robot+"/" + action_root + "/cancel")
+
+				self.defaultPullsTopics[self.commander_machine].append(robot+"/" + action_root + "/status")
+				self.defaultPullsGateway[self.commander_machine].append(self.pr2_machine)
+				self.defaultPullsTopics[self.commander_machine].append(robot+"/" + action_root + "/result")
+				self.defaultPullsGateway[self.commander_machine].append(self.pr2_machine)
+				self.defaultPullsTopics[self.commander_machine].append(robot+"/" + action_root + "/feedback")
+				self.defaultPullsGateway[self.commander_machine].append(self.pr2_machine)
+
 
 
 		# next add the list of default advertisements from publsihers
@@ -78,7 +133,7 @@ class MultimasterConfigGenerator:
 			for publisher_topic in publisher_topics:
 				self.defaultAdvertisements[gateway].append(prefix + publisher_topic)
 
-		IPython.embed()
+
 		# add the list of default pulls from publishers
 		for gateway in self.gateway_list:
 			for other_gateway in self.gateway_list:
@@ -88,8 +143,9 @@ class MultimasterConfigGenerator:
 						prefix = self.robot_dict[other_gateway]
 					topics = rospy.get_param("generator_"+other_gateway+"_publishers")
 					for topic in topics:
-						self.defaultPulls[gateway].append(prefix + topic)
-		IPython.embed()
+						self.defaultPullsTopics[gateway].append(prefix + topic)
+						self.defaultPullsGateway[gateway].append(other_gateway)
+
 		# Write the accumulated advertisements and pull to config file
 		for gateway in self.gateway_list:
 			rospy.loginfo("Creating configuration for " + gateway)
@@ -100,6 +156,16 @@ class MultimasterConfigGenerator:
 			file.close()
 
 		# write the remapping topic to the generated launch file
+		gateway = self.pr2_machine
+		robot_prefix = "pr2"
+		file = open(self.package_path + "/launch/generated/" + gateway+"_remappings.launch", 'w')
+		file.write("<launch>\n")
+
+		self.writePublishedRemaps(file, gateway)
+		self.writeSubscribedRemaps(file, gateway, robot_prefix)
+
+		file.write("</launch>")
+		file.close()
 
 
 if __name__ == '__main__':
